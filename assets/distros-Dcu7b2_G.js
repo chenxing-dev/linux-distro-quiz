86930b78699fe8cfec6a161990851ef3510688b1
chenxing-dev <dev.chenxing@gmail.com>
2025-06-18 00:09:51 +0800

    Updates

--- assets/distros-Dcu7b2_G.js
+++ assets/distros-Dcu7b2_G.js
@@ -1,4 +1,4 @@
-import{j as M}from"./ui-COY6ZR7s.js";const e=[{id:"macos",name:"macOS",description:"You prioritize aesthetics over functionality and have more money than sense. You enjoy paying the Apple Tax™ and being locked into an ecosystem.",traits:["wealthy","aesthetic-driven","ecosystem-locked"],ascii:`
+import{j as M}from"./ui-D6mPz056.js";const e=[{id:"macos",name:"macOS",description:"You prioritize aesthetics over functionality and have more money than sense. You enjoy paying the Apple Tax™ and being locked into an ecosystem.",traits:["wealthy","aesthetic-driven","ecosystem-locked"],ascii:`
                      ..'
                  ,xNMM.
                .OMMMMo
